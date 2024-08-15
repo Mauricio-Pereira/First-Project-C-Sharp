963d4c898ca016455f433dd3b752a2586c9ee563
Mauricio Pereira <mauricio.pvieira1@gmail.com>
2024-08-15 11:51:55 -0300

    practicing for first cp

---
+++ IniciandoComCsharp_2TDSPC/CP01-TEST/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using CP01_TEST;
+using HtmlAgilityPack;
+
+var urlMain = "https://scryfall.com/sets/dsk";
+var urls = new List<string>();
+var cardsName = new List<string>();
+
+
+var html = new HtmlWeb().LoadFromWebAsync(urlMain).Result;
+var mainDiv = html.DocumentNode.SelectNodes("//*[@class='card-grid-inner']");
+
+// Seleciona todos os links <a> dentro das divs que estão dentro da div principal
+foreach (var node in mainDiv)
+{
+    urls.AddRange(node.Descendants("a").Select(a => a.GetAttributeValue("href", string.Empty)));
+}
+
+Parallel.ForEach(urls, link =>
+{
+    var htmlCard = new HtmlWeb().LoadFromWebAsync(link).Result;
+
+    var cardName = htmlCard.DocumentNode.SelectSingleNode("/html/body/div[3]/div[1]/div/div[3]/h1/span[1]");
+    var cardDesc = htmlCard.DocumentNode.SelectSingleNode("//*[@class='card-text-box']");
+    // Seleciona todos os parágrafos dentro do cardDescNode
+    var paragraphs = cardDesc.SelectNodes(".//p");
+    
+    // Concatena o texto de todos os parágrafos
+    var cardDescText = string.Join(" ", paragraphs.Select(p => p.InnerText.Trim()));
+    cardsName.Add(cardDesc.InnerText.Trim());
+    Console.WriteLine(link);
+    html = null;
+    GC.Collect();
+});
+
+
+foreach (var name  in cardsName)
+{
+    Console.WriteLine(name);
+}
+
+var csv = new StringBuilder();
+csv.AppendLine("Name");
+csv.AppendLine(string.Join("\n", cardsName.Select(g => $"{g}")));
+File.WriteAllText("cards.csv", csv.ToString());
---
+++ IniciandoComCsharp_2TDSPC/CP01-TEST/SwCards.cs
@@ -0,0 +1,8 @@
+﻿namespace CP01_TEST;
+
+public record SwCards( string ? Nome, string ? Descricao )
+{
+    public SwCards(string? Nome) : this(Nome, "")
+    {
+    }
+};
